7e2b7320016c06623dfcf09ddc14b11bbb19a987
Stefaan <safwebdev@gmail.com>
2019-10-16 15:58:54 +0800

    update model & controller

--- routes/user/model.js
+++ routes/user/model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const userSchema = mongoose.Schema({
     firstname: {
         type: String,
@@ -15,12 +17,37 @@ const userSchema = mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true
     },
     account_created: {
         type: String,
-        default: Date.now(),
+        default: Date.now()
     }
 });
+
+
+userSchema.pre('save', function (next) {
+    let user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+}
 
 const userModel = mongoose.model('user', userSchema);
 
